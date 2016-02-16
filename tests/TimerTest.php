86e557f2445002da2c64f72f1b00e34b44441790
Sergey Sorokin <isswp101@gmail.com>
2016-02-16 23:01:11 +0300

    Add some tests

--- tests/TimerTest.php
+++ tests/TimerTest.php
@@ -9,4 +9,28 @@ class TimerTest extends PHPUnit_Framework_TestCase
         $timer = new Timer;
         $this->assertRegExp('/\d{2}:\d{2}:\d{2}\.\d{3}/', $timer->end());
     }
+
+    public function testTimerFormatWithoutHours()
+    {
+        $timer = new Timer('i:s');
+        $this->assertRegExp('/\d{2}:\d{2}\.\d{3}/', $timer->end());
+    }
+
+    public function testTimerFormatWithoutHoursAndMinutes()
+    {
+        $timer = new Timer('s');
+        $this->assertRegExp('/\d{2}\.\d{3}/', $timer->end());
+    }
+
+    public function testEmptyTimerFormat()
+    {
+        $timer = new Timer('');
+        $this->assertRegExp('/\.\d{3}/', $timer->end());
+    }
+
+    public function testCustomTimerFormat()
+    {
+        $timer = new Timer('H-i-s');
+        $this->assertRegExp('/\d{2}-\d{2}-\d{2}\.\d{3}/', $timer->end());
+    }
 }
